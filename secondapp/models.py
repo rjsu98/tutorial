edeae08dbf9eb9de857b65ba7fbf9d6188eab982
jisoo <rjsu98@naver.com>
2020-07-30 13:56:09 +0900

    123

--- secondapp/models.py
+++ secondapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
+# 마이그레이션 작업 시 자동으로 앱의 이름이 secondapp_hospital로 붙게됨.
 class Hospital(models.Model):
+#  id = models.CharField(max_length=255) , 생략가능, 자동생성
  sido = models.CharField(max_length=255)
  name = models.CharField(max_length=255)
  medical = models.CharField(max_length=255)
